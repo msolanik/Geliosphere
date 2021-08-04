2e1ec54055b59721c57f868193d888bd6f00a506
Michal Solanik <22960818+msolanik@users.noreply.github.com>
2021-08-04 12:37:35 +0200

    Added CLI lib

---
+++ lib/include/CLI/CLI.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// Distributed under the 3-Clause BSD License.  See accompanying
+// file LICENSE or https://github.com/CLIUtils/CLI11 for details.
+
+// CLI Library includes
+// Order is important for combiner script
+
+#include "Version.hpp"
+
+#include "StringTools.hpp"
+
+#include "Error.hpp"
+
+#include "TypeTools.hpp"
+
+#include "Split.hpp"
+
+#include "Ini.hpp"
+
+#include "Validators.hpp"
+
+#include "Option.hpp"
+
+#include "App.hpp"
