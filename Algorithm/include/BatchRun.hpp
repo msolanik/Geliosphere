905a1f16452afd93dd1a987f9066f72146fd29f8
Tomáš Telepčák <tomas.telepcak@student.tuke.sk>
2024-01-21 21:55:24 +0100

    added header in BatchRun.hpp, utilizing RapidCSV, input file for Batch run moved to root of the project

--- Algorithm/include/BatchRun.hpp
+++ Algorithm/include/BatchRun.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file BatchRun.hpp
+ * @author Tomas Telepcak
+ * @brief Abstract definition for algorithm
+ * @version 0.1
+ * @date 2024-01-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef BATCH_RUN
 #define BATCH_RUN
 
--- Algorithm/src/BatchRun.cpp
+++ Algorithm/src/BatchRun.cpp
@@ -12,67 +12,10 @@
 #include <cstdlib>
 #include <filesystem>
 #include "GeliosphereAlgorithm.hpp"
+#include "rapidcsv.h"
 
 
 using namespace std;
-
-std::vector<std::string> splitString(const std::string &s, char delimiter) {
-    std::vector<std::string> tokens;
-    std::istringstream tokenStream(s);
-    std::string token;
-    while (std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
-
-std::vector<std::vector<std::string>> readFile(ParamsCarrier *singleTone){
-    std::ifstream file("../Algorithm/src/geliosphere_batch_input_paramaters.csv");
-
-    // Check if the file is open
-    if (!file.is_open()) {
-        std::cerr << "Error opening the file." << std::endl;
-        return std::vector<std::vector<std::string>>();;
-    }
-
-    // Vector of vectors to store the loaded integers
-    std::vector<std::vector<std::string>> data;
-    
-    // Read and process each line
-    std::string line;
-    std::getline(file, line);
-    while (std::getline(file, line)) {
-        // Split the line into fields
-        std::vector<std::string> fields = splitString(line, ',');
-
-        std::vector<std::string> row;
-        // Process each field
-        for (const std::string &field : fields) {
-            row.push_back(field);
-            // Do something with the field
-            //std::cout << field << " ";
-        }
-        data.push_back(row);
-        // Move to the next line
-        //std::cout << std::endl;
-    }
-
-    // Close the file
-    file.close();
-
-    // Print the loaded 2D array
-    /*std::cout << "Loaded 2D array:" << std::endl;
-    for (const auto &row : data) {
-        for (std::string value : row) {
-            std::cout << value << " ";
-        }
-        std::cout << std::endl;
-    }
-    */
-    return data;
-
-
-}
 
 void generateTomlFile(double r, double theta) {
     //std::cout << "r: " << r << " theta: " << theta << std::endl;
@@ -101,11 +44,6 @@ void generateTomlFile(double r, double theta) {
             { "remove_log_files_after_simulation", true } } 
         }
     };
-    //std::cout << toml::toml_formatter{ tbl } << "\n";
-    //std::cout << tbl << "\n";
-    // Save to TOML file
-    //std::remove("./Settings_batch.toml");
-    //std::cout << "Snazim sa zapisat do Toml settings" << std::endl;
     std::ofstream file("./Settings_batch.toml");
     
     if (file.is_open()) {
@@ -119,44 +57,50 @@ void generateTomlFile(double r, double theta) {
 
 void BatchRun::runAlgorithm(ParamsCarrier *singleTone)
 {
-    //std::string currentApplicationPath = getApplicationPath(argv);
-    //std::cout << "Hello, World! , value> " << singleTone->getString("inputFile", "test") << std::endl;
-    //std::ifstream file(singleTone->getString("inputFile", "geliosphere_batch_input_paramaters.csv"));
+    //rapidcsv::Document doc("../Algorithm/src/geliosphere_batch_input_paramaters.csv", rapidcsv::LabelParams(0, -1));
+    rapidcsv::Document doc("../geliosphere_batch_input_paramaters.csv", rapidcsv::LabelParams(0, -1));
+    
+    std::cout << "velkost nacitaneho suboru" << doc.GetRowCount() <<std::endl;
+    std::cout << "pocet stlpcov" << doc.GetColumnCount() <<std::endl;
+    
+    std::vector<std::string> row = doc.GetRow<std::string>(125);
+    std::cout << "test riadkov : " << row[4] << std::endl;      
+    
     ParseParams *parse = new ParseParams();
-    std::vector<std::vector<std::string>> data = readFile(singleTone);
-	
+    
     singleTone->putInt("csv", 1);
     singleTone->putString("model", "2D Geliosphere");
     parse->dtSetCheck(singleTone,1000);
-    //ParamsCarrier* single = singleTone->instance();
     std::cout << "singletone inner things: " << singleTone->getString("model", "daco nedobre") <<std::endl;
-    std::size_t numRows = data.size();
-    std::cout << "Number of rows: " << numRows << std::endl;
     std::string  targetDirectory= "test";    
     int test = 0;
+    int opakovanie = 0;
 
-    for (int i = 0; i < numRows; i++) {
-        if(std::stod(data[i][0]) >= 1997 && std::stod(data[i][0]) <= 1998){
-            generateTomlFile(std::stod(data[i][3]),std::stod(data[i][4]));
+    for (int i = 0; i < doc.GetRowCount() ; i++) {
+        std::vector<std::string> data = doc.GetRow<std::string>(i);
+        if(std::stod(data[0]) >= 1997 && std::stod(data[0]) <= 1998){
+            generateTomlFile(std::stod(data[3]),std::stod(data[4]));
             parse->newSettingsLocationCheck(singleTone, "./Settings_batch.toml");
             //std::cout << std::filesystem::absolute("./") << std::endl;
-            parse->monthYearCheck(singleTone, std::stod(data[i][0]), std::stod(data[i][1]), "./" );
-            targetDirectory = data[i][0] + "_" + data[i][1] + "_" + data[i][2];
+            parse->monthYearCheck(singleTone, std::stod(data[0]), std::stod(data[1]), "./" );
+            targetDirectory = data[0] + "_" + data[1] + "_" + data[2];
             singleTone->putString("destination", targetDirectory);
-            std::cout << "Destination: " << singleTone->getString("destination", "test") << " : " << data[i][0] << "_" << data[i][1] << "_" << data[i][2] << std::endl;
+            std::cout << "Destination: " << singleTone->getString("destination", "test") << " : " << data[0] << "_" << data[1] << "_" << data[2] << std::endl;
             std::cout << "Model: " << singleTone->getString("model", "test") << std::endl;
             AbstractAlgorithm *actualAlgorithm = new GeliosphereAlgorithm();
             actualAlgorithm->runAlgorithm(singleTone);
 
             for (int j = 0; j < 5; j++){    
-                std::cout << " " << data[i][j];
+                std::cout << " " << data[j];
             }
             std::cout << std::endl;
             test++;
             chdir("../../..");
         }
+        opakovanie = i ;
     }
     std::cout << "Test: " << test << std::endl;
+    std::cout << "I : " << opakovanie << std::endl;
 }
 
 
